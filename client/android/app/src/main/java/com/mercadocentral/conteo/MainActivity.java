329b52099d332f4dcbf3d633de1c4fe2e0d98bf4
Nrlsb <luksbenitez01@hotmail.com>
2026-02-11 11:41:47 -0300

    Actualizacion 1.0.454

--- client/android/app/src/main/java/com/mercadocentral/conteo/MainActivity.java
+++ client/android/app/src/main/java/com/mercadocentral/conteo/MainActivity.java
@@ -2,4 +2,10 @@ package com.mercadocentral.conteo;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(com.getcapacitor.community.speechrecognition.SpeechRecognition.class);
+        super.onCreate(savedInstanceState);
+    }
+}
