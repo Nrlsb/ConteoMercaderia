67829f19d0a1a24b33a173acb627c718a646eb84
Nrlsb <luksbenitez01@hotmail.com>
2026-04-21 17:00:55 -0300

    Actualizacion 1.0.689

--- client/src/api.js
+++ client/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL || '/',
-    timeout: 20000 // 20 seconds
+    timeout: 60000 // 60 seconds
 });
 
 api.interceptors.request.use(
