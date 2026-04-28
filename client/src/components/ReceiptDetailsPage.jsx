d7dd73e59d0c114f32dd8561b66eb73e0ebee263
Nrlsb <luksbenitez01@hotmail.com>
2026-04-28 16:53:13 -0300

    Actualizacion 1.0.076

--- client/src/components/ReceiptDetailsPage.jsx
+++ client/src/components/ReceiptDetailsPage.jsx
@@ -1,5 +1,5 @@
 
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useMemo } from 'react';
 import ReactDOM from 'react-dom';
 import { useParams, useNavigate } from 'react-router-dom';
 
